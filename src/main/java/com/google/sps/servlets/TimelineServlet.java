39ae40f470b5119c75196449ba3ec0b84f85ba97
Olivia Smith <osmith@sps-program.com>
2020-07-07 08:18:57 -0500

    Servlet compiles and works!

--- src/main/java/com/google/sps/servlets/TimelineServlet.java
+++ src/main/java/com/google/sps/servlets/TimelineServlet.java
@@ -12,9 +12,6 @@
 
 package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
 import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -29,21 +26,21 @@ public class TimelineServlet extends HttpServlet {
 
   public TimelineServlet() {}
 
-  class TimelineNode {
-      public String trickName;
+    class TrickNode {
+      public String trick_name;
       public String date;
-      public String mediaLink;
-      public String notes
+      public String link;
+      public String notes;
 
-      public TimelineNode(String trickName, String date, String mediaLink, String notes) {
-          this.trickName = trickName;
+      public TrickNode(String trick_name, String date, String link, String notes) {
+          this.trick_name = trick_name;
           this.date = date;
-          this.mediaLink = mediaLink;
+          this.link = link;
           this.notes = notes;
       }
   }
 
-  private String toGson(ArrayList<TimelineNode> tricks) {
+  private String toGson(ArrayList<TrickNode> tricks) {
     Gson gson = new Gson();
     String json = gson.toJson(tricks);
     return json;
@@ -54,14 +51,14 @@ public class TimelineServlet extends HttpServlet {
     // a function that will handle retrieving a user's tricks
     // for now we only send dummy data
     response.setContentType("application/json;");
-    ArrayList<TimelineNode> timelineNodes = new ArrayList<TimelineNode>();
+    ArrayList<TrickNode> tricks = new ArrayList<TrickNode>();
 
-    timelineNodes.add(new timelineNode("Shuvit", "2018-10-25T03:24:00", "https://www.youtube.com/watch?v=5gJl69sBRnw", "Didn't take long!"));
-    timelineNodes.add(new timelineNode("Ollie", "2018-10-27T06:10:59", "https://www.youtube.com/watch?v=851cTIcNuDU", "Easier said than done"));
-    timelineNodes.add(new timelineNode("Nollie", "2018-10-30T11:42:22", "https://www.youtube.com/watch?v=Vj9OIDO3uuU", "*shivers*"));
-    timelineNodes.add(new timelineNode("Pop Shuvit", "2018-11-10T10:01:43", "https://www.youtube.com/watch?v=XmuBAdiSvbE", "I'm flying!"));
-    timelineNodes.add(new timelineNode("KickFlip", "2019-02-23T03:45:02", "https://www.youtube.com/watch?v=OsXOGs17PAs", "Thanks Bobby for the moral support."));
+    tricks.add(new TrickNode("Shuvit", "2018-10-25T03:24:00", "https://www.youtube.com/watch?v=5gJl69sBRnw", "Didn't take long!"));
+    tricks.add(new TrickNode("Ollie", "2018-10-27T06:10:59", "https://www.youtube.com/watch?v=851cTIcNuDU", "Easier said than done"));
+    tricks.add(new TrickNode("Nollie", "2018-10-30T11:42:22", "https://www.youtube.com/watch?v=Vj9OIDO3uuU", "*shivers*"));
+    tricks.add(new TrickNode("Pop Shuvit", "2018-11-10T10:01:43", "https://www.youtube.com/watch?v=XmuBAdiSvbE", "I'm flying!"));
+    tricks.add(new TrickNode("KickFlip", "2019-02-23T03:45:02", "https://www.youtube.com/watch?v=OsXOGs17PAs", "Thanks Bobby for the moral support."));
 
-    response.getWriter().println(toGson(timelineNodes));
+    response.getWriter().println(toGson(tricks));
   }
 }
