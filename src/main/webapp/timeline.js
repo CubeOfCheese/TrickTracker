e07854da18863a00c5d6557cf492641a6f64c5bd
micaylamendoza <micaylamendoza@gmail.com>
2020-07-18 14:58:13 -0700

    integrated timeline servlet with functions that print the dummy data

--- src/main/webapp/timeline.js
+++ src/main/webapp/timeline.js
@@ -18,20 +18,39 @@ function createTrickElement(trick) {
   trickElement.TrickNode = 'trick-container';
 
   const trickNameElement = document.createElement('span');
-  trickElement.innerText = trick.trick_name;
+  trickNameElement.innerText = trick.trick_name;
 
   const dateElement = document.createElement('span');
-  trickElement.innerText = trick.date;
+  dateElement.innerText = trick.date;
 
   const linkElement = document.createElement('span');
-  trickElement.innerText = trick.link;
+  linkElement.innerText = trick.link;
 
   const notesElement = document.createElement('span');
-  trickElement.innerText = trick.notes;
+  notesElement.innerText = trick.notes;
 
+  var trickTitle = document.createTextNode("Trick: ");
+  var dateTitle = document.createTextNode("Date: ");
+  var linkTitle = document.createTextNode("Link: ");
+  var notesTitle = document.createTextNode("Notes: ");
+  var br1 = document.createElement("br");
+  var br2 = document.createElement("br");
+  var br3 = document.createElement("br");
+  var br4 = document.createElement("br");
+  var br5 = document.createElement("br");
+
+  trickElement.appendChild(trickTitle);
   trickElement.appendChild(trickNameElement);
+  trickElement.appendChild(br1);
+  trickElement.appendChild(dateTitle);
   trickElement.appendChild(dateElement);
+  trickElement.appendChild(br2);
+  trickElement.appendChild(linkTitle);
   trickElement.appendChild(linkElement);
+  trickElement.appendChild(br3);
+  trickElement.appendChild(notesTitle);
   trickElement.appendChild(notesElement);
+  trickElement.appendChild(br4);
+  trickElement.appendChild(br5);
   return trickElement;
 }
