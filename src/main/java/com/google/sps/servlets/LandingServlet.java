b949966e0d82cf765310e315199caa395dd796e7
Ruben Sanchez <61813857+therubensan@users.noreply.github.com>
2020-07-19 16:04:59 -0700

    Add login and bio frontend/backend functionality (#13)
    * Add login/dropin page/servlet and bio servlet
    
    * Update servlet syntax errors
    
    * Add edit bio html and update backend servlets
    
    * Updated profile photo uploading mechanism

---
+++ src/main/java/com/google/sps/servlets/LandingServlet.java
@@ -0,0 +1,68 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.PrintWriter;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+@WebServlet("/landing")
+public class LandingServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html"); 
+    UserService userService = UserServiceFactory.getUserService();
+
+    if (!userService.isUserLoggedIn()) {
+        String loginURL = userService.createLoginURL("/dropin.html");
+        response.getWriter().println(loginURL);
+        return; 
+    }
+
+    boolean hasBiography = hasBiography(userService.getCurrentUser().getUserId()); 
+    if (hasBiography) {
+        response.sendRedirect("/home.html"); 
+    } else {
+        response.sendRedirect("/bio.html"); 
+    }
+  }
+
+  private boolean hasBiography(String id) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService(); 
+    Query query =
+        new Query("UserBiography")
+            .setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, id));
+    PreparedQuery results = datastore.prepare(query);
+    Entity entity = results.asSingleEntity();
+    if (entity == null) {
+        return false; 
+    }
+    return true;       
+  }
+
+}
