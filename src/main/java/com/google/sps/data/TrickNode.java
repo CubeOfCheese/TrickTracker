8105296483fc663f18206844ba15b9db19759bbd
Olivia Smith <osmith@sps-program.com>
2020-07-23 13:20:48 -0500

    skate sstyle integration

--- src/main/java/com/google/sps/data/TrickNode.java
+++ src/main/java/com/google/sps/data/TrickNode.java
@@ -2,15 +2,18 @@ package com.google.sps.data;
 
 // a class that holds data pertaining to a trick
 public class TrickNode {
+  public String skate_style;
   public String trick_name;
   public long date;
   public String link;
   public String notes;
 
-  public TrickNode(String trick_name, long date, String link, String notes) {
+  public TrickNode(String trick_name, long date, String link, String notes, String skate_style) {
     this.trick_name = trick_name;
     this.date = date;
     this.link = link;
     this.notes = notes;
+    this.skate_style = skate_style;
+
   }
 }
